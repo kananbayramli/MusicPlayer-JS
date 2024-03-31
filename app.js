e2fb7844d642eef0523649fafc85b1f0b0486126
kananbayramli <bayramli-kenan@mail.ru>
2024-03-31 12:51:47 +0400

    Music player controls

--- app.js
+++ app.js
@@ -21,5 +21,43 @@ function displayMusic(music){
 }
 
 play.addEventListener("click", () =>{
-    audio.play();
+    const isMusicPlay = container.classList.contains("playing");
+    isMusicPlay ? pauseMusic() : playMusic();
 });
+
+prev.addEventListener("click", () =>{
+    prevMusic();
+});
+
+next.addEventListener("click", () =>{
+    nextMusic();
+});
+
+
+function prevMusic(){
+    player.prev();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+function nextMusic(){
+    player.next();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+
+
+function pauseMusic(){
+    container.classList.remove("playing");
+    play.classList = "fa-solid fa-play";
+    audio.pause();
+}
+
+function playMusic(){
+    container.classList.add("playing");
+    play.classList = "fa-solid fa-pause";
+    audio.play();
+}
